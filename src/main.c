8472c5f4a39a73cf0f33d333b1df890ae0120c82
Michael Anderson <drmikeando@gmail.com>
2014-07-25 09:49:29 +0800

    Addeded stubby work_queue implementation and refactored.

--- src/main.c
+++ src/main.c
@@ -51,23 +51,44 @@ void message( logger * l, const char * fmt, ...) {
 	pthread_mutex_unlock(l->lock);
 }
 
+struct work_queue_entry {
+	//TODO: Not sure this is usefull .. but need some stub filler in there for now.
+	const char * name;
+};
+
+typedef struct work_queue_entry work_queue_entry;
+
 struct work_queue {
 	pthread_mutex_t lock;
 	int poisoned;
+	//TODO: This needs a lot of work .. at the moment we just add entries and increment a cursor.
+	// And the data is not initialized below yet.
+	// Later we'll need to grow/shrink this dynamically.
+	int max_entries;
+	int write_cursor;
+	int read_cursor;
+	work_queue_entry** entries;
 };
 
 typedef struct work_queue work_queue;
+
 
 void work_queue_create(work_queue **q) {
 	work_queue * retval = malloc(sizeof(work_queue));
 	memset(retval, 0, sizeof(work_queue));
 	retval->poisoned = 0;
 	pthread_mutex_init(&retval->lock, NULL);
+
+	retval->entries = malloc(sizeof(work_queue_entry*)*10);
+	retval->max_entries = 10;
+	retval->read_cursor = 0;  // Where the next read will read from
+	retval->write_cursor = 0; // Where the next add will write to
 	*q = retval;
 };
 
 void work_queue_destroy(work_queue *q) {
 	pthread_mutex_destroy(&q->lock);
+	free(q->entries);
 	free(q);
 };
 
@@ -76,6 +97,34 @@ void work_queue_poison(work_queue *q) {
 	q->poisoned = 1;
 	pthread_mutex_unlock(&q->lock);
 };
+
+void work_queue_add(work_queue *q, work_queue_entry * entry);
+
+void work_queue_add(work_queue * q, work_queue_entry * entry ) {
+	//TODO: Report errors better
+	if(q->write_cursor >= q->max_entries-1 ) {
+		printf("OMG I IZ TOAST.\n");
+		return;
+	}
+
+	q->entries[q->write_cursor] = entry;
+	q->write_cursor++;
+}
+
+
+
+//TODO: These action creators are naf.
+work_queue_entry * work_queue_create_action(const char * name) {
+	work_queue_entry * retval = malloc(sizeof(work_queue_entry));
+	retval->name = name;
+	return retval;
+}
+
+work_queue_entry * work_queue_create_actionX(const char * name, work_queue * origin) {
+	work_queue_entry * retval = malloc(sizeof(work_queue_entry));
+	retval->name = name;
+	return retval;
+}
 
 //TODO: Create a logging function (and maybe a logging thread?)
 
@@ -100,12 +149,18 @@ struct git_remote_thread_data {
 
 typedef struct git_remote_thread_data git_remote_thread_data;
 
-void* sqlite_thread_fn(void * data) {
-	sqlite_thread_data * d = data;
-	work_queue * q = d->queue;
+
+struct worker {
+	work_queue * queue;
+	const char * name;
+	logger * root_logger;
+};
+
+void run_worker( struct worker * w ) {
+	work_queue * q = w->queue;
 	logger * logx;
-	logger_init_derived(&logx, d->root_logger, "SQLITE3");
-	message(logx," --- In sqlite thread\n");
+	logger_init_derived(&logx, w->root_logger, w->name);
+	message(logx," --- Started worker thread\n");
 	while(1) {
 		if(pthread_mutex_trylock(&q->lock)==0) {
 			if(q->poisoned==1) {
@@ -113,62 +168,45 @@ void* sqlite_thread_fn(void * data) {
 				break;
 			}
 			message(logx,"Trying to consume stuff off my work queue\n");
-			pthread_mutex_unlock(&q->lock);
+			if(q->read_cursor < q->write_cursor) {
+				message(logx, "I have an entry\n");
+				work_queue_entry * entry = q->entries[q->read_cursor];
+				q->read_cursor++;
+				pthread_mutex_unlock(&q->lock);
+
+				message(logx,"My entry is : %s\n", entry->name);
+
+			} else {
+				message(logx,"My queue is empty...\n");
+				pthread_mutex_unlock(&q->lock);
+			}
 		} else {
 			message(logx,"Spinning...\n");
 		}
 		usleep(10);
 	}
 
-	message(logx," --- sqlite thread exiting\n");
+	message(logx," --- worker thread exiting\n");
+}
+
+void* sqlite_thread_fn(void * data) {
+	sqlite_thread_data * d = data;
+	struct worker w = { d->queue, "SQLITE3", d->root_logger };
+	run_worker(&w);
 	return NULL;
 }
 
 void* git_local_thread_fn(void * data) {
 	git_local_thread_data * d = data;
-	work_queue * q = d->queue;
-	logger * logx;
-	logger_init_derived(&logx, d->root_logger, "GIT_L");
-	message(logx," --- In git local thread\n");
-	while(1) {
-		if(pthread_mutex_trylock(&q->lock)==0) {
-			if(q->poisoned==1) {
-				pthread_mutex_unlock(&q->lock);
-				break;
-			}
-			message(logx,"Trying to consume stuff off my work queue\n");
-			pthread_mutex_unlock(&q->lock);
-		} else {
-			message(logx,"Spinning...\n");
-		}
-		usleep(10);
-	}
-
-	message(logx," --- git local thread exiting\n");
+	struct worker w = { d->queue, "GIT_L", d->root_logger };
+	run_worker(&w);
 	return NULL;
 }
 
 void* git_remote_thread_fn(void * data) {
 	git_remote_thread_data * d = data;
-	work_queue * q = d->queue;
-	logger * logx;
-	logger_init_derived(&logx, d->root_logger, "GIT_R");
-	message(logx," --- In git remote thread\n");
-	while(1) {
-		if(pthread_mutex_trylock(&q->lock)==0) {
-			if(q->poisoned==1) {
-				pthread_mutex_unlock(&q->lock);
-				break;
-			}
-			message(logx,"Trying to consume stuff off my work queue\n");
-			pthread_mutex_unlock(&q->lock);
-		} else {
-			message(logx,"Spinning...\n");
-		}
-		usleep(10);
-	}
-
-	message(logx," --- git remote thread exiting\n");
+	struct worker w = { d->queue, "GIT_R", d->root_logger };
+	run_worker(&w);
 	return NULL;
 }
 
@@ -228,12 +266,24 @@ int main() {
 	pthread_t git_remote_thread;
 	pthread_create(&git_remote_thread, NULL, &git_remote_thread_fn, &git_remote_data);
 
+	work_queue * main_queue;
+	work_queue_create(&main_queue);
+
 
 	message(root_logger,"Telling remote git to refresh...\n");
+	
+	work_queue_add(git_remote_queue, work_queue_create_action("Refresh"));
 
 	message(root_logger,"Fetching an object from sqlite3\n");
-	message(root_logger,"Updating object\n");
+	
+	//It needs to know where to stick the object once its loaded - thats the main queue.
+	work_queue_add(sqlite3_queue, work_queue_create_actionX("Fetch Object", main_queue) );
+
+	message(root_logger,"TODO: Updating object\n");
+	// Wait for the response on the main queue.
+	
 	message(root_logger,"Saving object back to sqlite3\n");
+	work_queue_add(sqlite3_queue, work_queue_create_actionX("Save Object", main_queue) );
 
 	message(root_logger,"Getting local git changes\n");
 	message(root_logger,"Telling local git to commit changes\n");
